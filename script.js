c3cb361cfa9c210b0b262dba0a0f850c276a2c7a
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-20 04:37:02 -0700

    hero section completed

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const hamburger = document.querySelector(".hamburger")
-const mobileMenu = document.querySelector(".nav-list ul")
-hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active")
-    mobileMenu.classList.toggle("active")
-})
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".nav-list ul");
+const menuItem = document.querySelectorAll(".nav-list ul li a");
+const header = document.querySelector(".header");
+console.log("header")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+});
+
+menuItem.forEach(item => {
+    item.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        mobileMenu.classList.remove("active");
+    });
+});
+
+document.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY; // Use window.scrollY to get the vertical scroll position
+    if (scrollPosition > 300) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = "transparent"; // Correct spelling here
+    }
+});
+
+// Smooth scrolling on anchor links
