a62b389c3e7a93622f0ab9a96de201b25bae7150
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-09-19 21:15:39 -0700

    completed styling header section

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger")
+const mobileMenu = document.querySelector(".nav-list ul")
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active")
+    mobileMenu.classList.toggle("active")
+})
